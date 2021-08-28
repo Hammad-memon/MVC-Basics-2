b6977e559b88d220e5a9817717d4887a0a16fffc
Hammad-memon <52237455+Hammad-memon@users.noreply.github.com>
2021-08-28 22:32:00 +0500

    I have update some file about feverchecker function

--- CodeAlong Template/Controllers/HomeController.cs
+++ CodeAlong Template/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using CodeAlong_Template.Models;
+using fever.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -33,11 +34,6 @@ namespace CodeAlong_Template.Controllers
             return View();
         }
 
-        public IActionResult FeverChecker()
-        {
-            return View();
-        }
-
         public IActionResult Project()
         {
             return View();
@@ -55,6 +51,66 @@ namespace CodeAlong_Template.Controllers
 
         public IActionResult Test()
         {
+            return View();
+        }
+
+
+        //[Route("/Home/FeverChecker")]
+        [HttpGet]
+        public ActionResult FeverChecker()
+        {
+            ViewData["Fever"] = "";
+            ViewData["shypothermia"] = "";
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        //[Route("/Home/FeverChecker")]
+        public ActionResult FeverChecker(Fever model)
+        {
+            if (model.Unit == "Fahrenheit")
+            {
+                if (model.CheckFever >= 99)
+                {
+                    ViewData["Message"] = "You have fever ";
+                }
+                else
+                {
+                    ViewData["Message"] = "You have not  fever ";
+                    if (model.Ishypothermia && (model.CheckFever <= 95))
+                    {
+                        ViewData["shypothermia"] = "but hypothermia";
+                    }
+                    else
+                    {
+                        ViewData["shypothermia"] = "";
+
+                    }
+                }
+            }
+            else
+            {
+                if (model.CheckFever >= 38)
+                {
+                    ViewData["Message"] = "You have fever ";
+                }
+                else
+                {
+                    ViewData["Message"] = "You have not  fever ";
+                    if (model.Ishypothermia && (model.CheckFever <= 35))
+                    {
+                        ViewData["shypothermia"] = "but hypothermia";
+                    }
+                    else
+                    {
+                        ViewData["shypothermia"] = "";
+                    }
+
+                }
+
+            }
             return View();
         }
         //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
